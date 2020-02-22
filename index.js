f7ba99de42c8834ba534fae8b2158341389a5723
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2020-02-22 14:15:26 +0000

    Add login logic (Closes #4), add registration logic (Closes #5)
    - [x] Setup session and cookie libraries
    - [x] Add a login GET handler
    - [x] Add a login POST handler
    - [x] Add error reporting facilities
    - [x] Add logout handling and session regeneration
    - [x] Add the user toolbar links with login names
    - [x] Add the GET handler for registration
    - [x] Add the POST handler for registration
    - [x] Add error checking for registration

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Sequelize = require('sequelize');
 const bcrypt = require('bcryptjs');
+const session = require('express-session');
 const server = express();
 require('dotenv').config();
 
@@ -49,18 +50,118 @@ Message.belongsTo(User);
 const port = process.env.PORT;
 
 server.set('view engine', 'ejs');
-server.use(express.static('public'))
+server.use(express.static('public'));
+server.use(express.urlencoded({ 'extended': true }));
+server.use(session({
+    'secret': process.env.SESSION_SECRET,
+    'resave': false,
+    'saveUninitialized': true
+}));
 
 server.get('/', (request, response) => {
-    response.render('index');
+    response.render('index', { 'session': request.session });
 });
 
 server.get('/login', (request, response) => {
-    response.render('login');
+    response.render('login', { 'session': request.session });
+});
+
+server.post('/login', (request, response) => {
+    const login = (request.body.login || '').trim();
+    if (!login) {
+        request.session.error = 'Login can not be empty.';
+        response.redirect('/login');
+        return;
+    }
+
+    const password = (request.body.password || '').trim();
+    if (!password) {
+        request.session.error = 'Password can not be empty.';
+        response.redirect('/login');
+        return;
+    }
+
+    User.findOne({ 'where': { login } }).then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+            request.session.authorized = true;
+            request.session.userID  = user.id;
+            request.session.login  = user.login;
+            request.session.administrator = user.administrator;
+            response.redirect('/');
+        } else {
+            request.session.error = 'Failed to login.';
+            response.redirect('/login');
+        }
+    }).catch(error => {
+        console.error(error);
+        response.status(503).end('Service Unavailable');
+    });
+});
+
+server.post('/logout', (request, response) => {
+    request.session.regenerate(error => {
+        if (error) {
+            console.error(error);
+        }
+
+        response.redirect('/');
+    });
 });
 
 server.get('/register', (request, response) => {
-    response.render('register');
+    response.render('register', { 'session': request.session });
+});
+
+server.post('/register', (request, response) => {
+    const login = (request.body.login || '').trim();
+    if (!login) {
+        request.session.error = 'Login can not be empty.';
+        response.redirect('/register');
+        return;
+    }
+
+    const password = (request.body.password || '').trim();
+    if (!password) {
+        request.session.error = 'Password can not be empty.';
+        response.redirect('/register');
+        return;
+    }
+
+    const passwordRepeat = (request.body['password-repeat'] || '').trim();
+    if (!passwordRepeat) {
+        request.session.error = 'Password has to be repeated twice.';
+        response.redirect('/register');
+        return;
+    }
+
+    if (password !== passwordRepeat) {
+        request.session.error = 'Password are not the same.';
+        response.redirect('/register');
+        return;
+    }
+
+    User.findOne({ 'where': { login } }).then(user => {
+        if (user) {
+            request.session.error = 'The login is occupied. Select anothe one.';
+            response.redirect('/register');
+        } else {
+            const hashedPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+            return User.create({
+                'login': login,
+                'password': hashedPass,
+                'administrator': false
+            }).then(user => {
+                request.session.authorized = true;
+                request.session.userID  = user.id;
+                request.session.login  = user.login;
+                request.session.administrator = user.administrator;
+                response.redirect('/');
+            });
+        }
+    }).catch(error => {
+        console.error(error);
+        response.status(503).end('Service Unavailable');
+    });
 });
 
 (function loop(){
