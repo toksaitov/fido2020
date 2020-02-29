d3051de9c6d1a63a92f26701f7c85d43a1ea9eb6
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2020-02-29 14:10:04 +0000

    Add message manipulation logic (Closes #7)
    - [x] Add the Delete button for messages
    - [x] Add the Delete request handling route
    - [x] Add the Edit button for messages
    - [x] Add a message editing page
    - [x] Add the Edit request handling routes

--- index.js
+++ index.js
@@ -193,8 +193,113 @@ server.post('/message/create', (request, response) => {
     }).then(message => {
         response.redirect('/');
     }).catch(error => {
+        if (error.name === 'SequelizeDatabaseError') {
+            request.session.error = "Incorrect message.";
+            response.redirect('/');
+        } else {
+            console.error(error);
+            response.status(503).end('Service Unavailable');
+        }
+    });
+});
+
+server.post('/message/:id/delete', (request, response) => {
+    if (!request.session.authorized) {
+        response.status(401).end('Unauthorized');
+        return;
+    }
+
+    const id = request.params.id;
+    if (!id) {
+        request.session.error = "The message to be deleted was not specified.";
+        response.redirect('/');
+        return;
+    }
+
+    Message.findOne({ 'where': { id } }).then(message => {
+        if (!message) {
+            request.session.error = "No message to delete.";
+            response.redirect('/');
+        } else if (!(request.session.administrator || request.session.userID === message.userId)) {
+            response.status(401).end('Unauthorized');
+        } else {
+            return message.destroy().then(() => {
+                response.redirect('/');
+            });
+        }
+    }).catch(error => {
         console.error(error);
         response.status(503).end('Service Unavailable');
+    });
+});
+
+server.get('/message/:id/edit', (request, response) => {
+    if (!request.session.authorized) {
+        response.status(401).end('Unauthorized');
+        return;
+    }
+
+    const id = request.params.id;
+    if (!id) {
+        request.session.error = "The message to be edited was not specified.";
+        response.redirect('/');
+        return;
+    }
+
+    Message.findOne({ 'where': { id } }).then(message => {
+        if (!message) {
+            request.session.error = "No message to edit.";
+            response.redirect('/');
+        } else if (!(request.session.administrator || request.session.userID === message.userId)) {
+            response.status(401).end('Unauthorized');
+        } else {
+            response.render('message-edit', { message, 'session': request.session });
+        }
+    }).catch(error => {
+        console.error(error);
+        response.status(503).end('Service Unavailable');
+    });
+});
+
+server.post('/message/:id/edit', (request, response) => {
+    if (!request.session.authorized) {
+        response.status(401).end('Unauthorized');
+        return;
+    }
+
+    const id = request.params.id;
+    if (!id) {
+        request.session.error = "The message to be edited was not specified.";
+        response.redirect('/');
+        return;
+    }
+
+    const content = request.body.content;
+    if (!content) {
+        request.session.error = "The message can't be empty.";
+        response.redirect(`/message/${parseInt(id)}/edit`);
+        return;
+    }
+
+    Message.findOne({ 'where': { id } }).then(message => {
+        if (!message) {
+            request.session.error = "No message to edit.";
+            response.redirect('/');
+        } else if (!(request.session.administrator || request.session.userID === message.userId)) {
+            response.status(401).end('Unauthorized');
+        } else {
+            return message.update({ content }).then(() => {
+                response.redirect('/');
+            });
+        }
+    }).catch(error => {
+        if (error.name === 'SequelizeDatabaseError') {
+            request.session.error = "Incorrect message.";
+            response.redirect(`/message/${parseInt(id)}/edit`);
+        } else {
+            console.error(error);
+            response.status(503).end('Service Unavailable');
+        }
     });
 });
 
