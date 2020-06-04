d83cfef067757cd94343728c150b66122b2ec229
Toksaitov Dmitrii Alexandrovich <dmitrii@toksaitov.com>
2020-06-04 20:43:29 +0600

    Add company copyright

--- fido2020-front/src/components/Footer.js
+++ fido2020-front/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () =>
     <footer>
         <nav className="navbar navbar-light bg-light">
-            <p className="mx-auto mt-3">&copy; 2021</p>
+            <p className="mx-auto mt-3">&copy; DEVCIT 2021</p>
         </nav>
     </footer>;
 
